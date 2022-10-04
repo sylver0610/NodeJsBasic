a878f7164824d938952ee3b3af319a50288e145f
sylver0610 <79132069+sylver0610@users.noreply.github.com>
2022-10-04 23:46:38 +0300

    connect database

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import express from "express"
 import configViewEngine from "./configs/viewEngine"
 import initWebRoute from './route/web'
+//
 
 require('dotenv').config()
 //const path = require('path')
--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from '../controller/homeController';
+
 let router = express.Router();
 
 const initWebRoute = (app) => {
